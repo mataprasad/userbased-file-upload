9bffbe4aed96cfdabedf3d70cdb3559d5b0f34d4
Mata Prasad Chauhan <mataprasad045@gmail.com>
2017-12-24 16:50:12 +0530

    changes

--- code/post/upload-file-post.php
+++ code/post/upload-file-post.php
@@ -15,7 +15,7 @@ $file_name_db = ABSPATH . '/content/uploads/'.$db_name;
 $description = $_POST["description"];
 $user = getUserFromSession(); 
 //$data = db_getUploadsByUserId($user->id);
-echo $size;
+//echo $size;
 if($size>0){
     move_uploaded_file($_FILES['upload']['tmp_name'], $file_name_db);
 $id = db_insertUploadDetails($name,$description,$size,$user->id,$type,$db_name);
